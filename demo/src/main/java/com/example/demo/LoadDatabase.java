931fe13d11c5955a4b6900e16cc6895cac86c19e
Carlsson David <David.Carlsson@csn.se>
2022-06-22 14:25:25 +0200

    adding files

---
+++ demo/src/main/java/com/example/demo/LoadDatabase.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(MessageRepository repository) {
+
+        return args -> {
+            log.info("Preloading" + repository.save(new Message("Viktiga datum för elever", "1/1-22, 3/7-22, 24/12-22", "www.csn.se/viktigadatumelever", "David Carlsson")));
+            log.info("Preloading" + repository.save(new Message("Viktiga datum för studenter", "1/1-22, 3/4-22, 31/8-22", "www.csn.se/viktigadatumstudenter", "Mikolaj Wagner")));
+        };
+    }
+}
