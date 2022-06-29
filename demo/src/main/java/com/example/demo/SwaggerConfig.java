44d4cddcb93ca87297442bf84211433aa8ff0d2c
Carlsson David <David.Carlsson@csn.se>
2022-06-29 14:22:35 +0200

    swagger working

--- demo/src/main/java/com/example/demo/SwaggerConfig.java
+++ demo/src/main/java/com/example/demo/SwaggerConfig.java
@@ -10,32 +10,32 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-
-/*
-import static com.google.common.collect.Lists.newArrayList;
-
-import java.util.Collections;
-
-import org.springframework.http.HttpMethod;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-*/
-
 
 @Configuration
-//@EnableSwagger2
 public class SwaggerConfig {
+  
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                    .select()
-                    .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
-                    .paths(PathSelectors.any())
-                    .build();
+        return new Docket(DocumentationType.SWAGGER_2)          
+          .apiInfo(apiInfo())
+          .select()    
+          .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+          .paths(PathSelectors.any())
+          .build();
     }
 
+    private ApiInfo apiInfo() {
+      return new ApiInfoBuilder()
+        .title("Open communication API created by CSN")
+        .description("To be used by CSN to spread information to stakeholders through external websites")
+        .termsOfServiceUrl("http://javainuse.com")
+        .contact(new Contact("David Carlsson", "", "david.carlsson@csn.se"))
+        .license("Apache License")
+          .licenseUrl("https://apache.org/licenses/LICENSE-2.0")
+        .version("1.0")
+        .build();
+    }
 }
