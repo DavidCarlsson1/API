688a70d6eac1b62a3e0d0dd09f8e8420fe248e3a
Carlsson David <David.Carlsson@csn.se>
2022-07-01 10:27:15 +0200

    openAPI 3.0 working

---
+++ demo/src/main/java/com/example/demo/OpenApiConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+ 
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()          
+          .info(new Info()
+                  .title("Open communication API created for CSN")
+                  .description("To be used by CSN for spreading information to stakeholders through external websites")
+                  .version("v1.0")
+                  .contact(new Contact()
+                          .name("David Carlsson & Mikolaj Wagner")
+                          .url("")
+                          .email("david.carlsson@csn.se & mikolaj.wagner@csn.se"))
+                  .termsOfService("https://www.csn.se/")
+                  .license(new License().name("Apache License").url("https://apache.org/licenses/LICENSE-2.0"))
+          );
+    }
+/*
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .setGroup("com.example.demo")
+                .pathsToMatch("/public/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .setGroup("springshop-admin")
+                .pathsToMatch("/admin/**")
+                .build();
+    }
+*/
+}
